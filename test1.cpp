b752bcfd227b17af99436e78331b52dbe8672640
xuewenzhang <xuewenzhang@tencent.com>
2025-08-13 15:20:47 +0800

    测试多个main文件独立构建运行

---
+++ test1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by zhangdashuai on 2025/8/13.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"test1"<<endl;
+    return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"test2"<<endl;
+    return 0;
+}
